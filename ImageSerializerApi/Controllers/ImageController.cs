0528a86067fadf119c098b375378b334c241d145
Adam Tovatt <sporedenstore@hotmail.com>
2022-11-13 04:31:41 +0100

    add line break att width of image bytes

--- ImageSerializerApi/Controllers/ImageController.cs
+++ ImageSerializerApi/Controllers/ImageController.cs
@@ -40,7 +40,7 @@ namespace ImageSerializerApi.Controllers
                 {
                     stringBuilder.Append(bytes[i]);
                     stringBuilder.Append(", ");
-                    if ((1 + i) % 8 == 0) stringBuilder.Append("\n");
+                    if ((1 + i) % imageFile.Width == 0) stringBuilder.Append("\n");
                 }
 
                 return new ApiResponse(new { content = stringBuilder.ToString() });
